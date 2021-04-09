8a2e0eccfdd5b14c4007df59c1d00dc18c27f4dc
Elijah Wheelock <>
2021-04-09 08:32:00 -0700

    disorganized commit

--- prefix.cu
+++ prefix.cu
@@ -15,21 +15,14 @@ void short_prefix(int *device_array, unsigned length) {
     dim3 threadsPerBlock(1024);
     for (unsigned step=1; step<length; step<<=1) {
         short_prefix_sum<<<numBlocks, threadsPerBlock>>>(device_array, length, step);
+        cudaDeviceSynchronize();
     }
 }
 
-// __global__ void long_prefix_sum(int *array, unsigned length, unsigned stride) {
-//     int i = blockIdx.x * blockDim.x + threadIdx.x;
-//     if ((i < length) && (stride <= i) && (0 == ((i+1) % stride))) {
-//         array[i] += array[i - stride];
-//     }
-//     __syncthreads();
-// }
-
-__global__ void long_prefix_upsweep(int *array, unsigned n, unsigned d) {
+__global__ void long_prefix_upsweep(int *array, unsigned length, unsigned d) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int p = (int) pow(2, d+1);
-    if ((i < n) && (0 == (i % p))) {
+    if ((i < length) && (0 == (i % p))) {
         array[i + p - 1] += array[i + p/2 - 1];
     }
     __syncthreads();
@@ -37,8 +30,11 @@ __global__ void long_prefix_upsweep(int *array, unsigned n, unsigned d) {
 
 __global__ void long_prefix_downsweep(int *array, unsigned length, unsigned d) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if ((i < length) && (stride >= i) && (1 == (i % stride))) {
-        array[i] += array[i - stride/2];
+    int p = (int) pow(2, d+1);
+    if ((i < length) && (0 == (i % p))) {
+        int tmp = array[i + p/2 - 1];
+        array[i + p/2 - 1] = array[i + p - 1];
+        array[i + p - 1] = tmp + array[i + p/2 - 1];
     }
     __syncthreads();
 }
@@ -46,20 +42,15 @@ __global__ void long_prefix_downsweep(int *array, unsigned length, unsigned d) {
 void long_prefix(int *device_array, unsigned n) {
     dim3 numBlocks(1);
     dim3 threadsPerBlock(1024);
- // unsigned stride;
-    int l = log(n)/log(2)
+    int  l = log(n)/log(2)
     for (int d=0; d < l; d++) {
         long_prefix_upsweep<<<numBlocks, threadsPerBlock>>>(device_array, n, d);
+        cudaDeviceSynchronize();
     }
- // long_prefix_sum<<<numBlocks, threadsPerBlock>>>(device_array, n, 1);
- // long_prefix_sum<<<numBlocks, threadsPerBlock>>>(device_array, n, 2);
     for (int d=l; d>=0; d--) {
-        long_prefix_sum<<<numBlocks, threadsPerBlock>>>(device_array, length, d);
- 
- // stride >>= 1;
- // for (; stride>1; stride>>=1) {
- //     long_prefix_fixup<<<numBlocks, threadsPerBlock>>>(device_array, n, stride);
- // }
+        long_prefix_downsweep<<<numBlocks, threadsPerBlock>>>(device_array, length, d);
+        cudaDeviceSynchronize();
+    }
 }
 
 int main(){
