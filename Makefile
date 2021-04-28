616b2205cbcd7b5f26d79cc011f922a8feb79dff
Elijah Wheelock <>
2021-04-28 10:35:01 -0700

    fix cpu implementation

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ gpu_prefix: prefix.cu
 	nvcc prefix.cu -o gpu_prefix.fatbin -Dsynchronize
 
 cpu_prefix: cpu_prefix.cc
-	g++ -g cpu_prefix.cc -o cpu_prefix.fatbin
+	g++ -g cpu_prefix.cc -o cpu_prefix.o
--- cpu_prefix.cc
+++ cpu_prefix.cc
@@ -4,25 +4,27 @@
 #include <errno.h>
 #include <assert.h>
 #include <string.h>
-#define log2(n) log(n)/log(2)
+#define LOG2(n) log(n)/log(2)
 
-void prefix_upsweep(int *array, unsigned length, unsigned d) {
-    for (unsigned i=0; i<length; ++i) {
-        unsigned p = 1 << (d+1);
-        if ((i < length) && (0 == (i % p))) {
-            array[i + p - 1] += array[i + p/2 - 1];
-        }
+void print_array(int *array, unsigned length) {
+    for (int i=0; i<length; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n\n");
+}
+
+void prefix_upsweep(int *array, unsigned length, unsigned stride) {
+    for (unsigned i=0; (i+stride)<=length; i+=stride) {
+        array[i + stride - 1] += array[i + stride/2 - 1];
     }
 }
 
-void prefix_downsweep(int *array, unsigned length, unsigned d) {
-    for (unsigned i=0; i<length; ++i) {
-        unsigned p = 1 << (d+1);
-        if ((i < length) && (0 == (i % p)) && ((i+p) < length)) {
-            int tmp = array[i + p/2 - 1];
-            array[i+p/2-1] = array[i+p-1];
-            array[i+p-1] = tmp + array[i+p/2-1];
-        }
+void prefix_downsweep(int *array, unsigned length, unsigned stride) {
+    for (unsigned i=0; 0<(i+stride/2) && (i+stride)<=length; i+=stride) {
+      //printf("%d %d -- %d: %d, %d: %d\n", i, stride, i+stride/2-1, array[i+stride/2], i+stride-1, array[i+stride-1]);
+        int tmp = array[i+stride/2-1];
+        array[i+stride/2-1] = array[i+stride  -1];
+        array[i+stride  -1] = array[i+stride/2-1] + tmp;
     }
 }
 
@@ -31,15 +33,18 @@ void prefix(int *host_array, unsigned length) {
     int *device_array; 
     assert(NULL != (device_array = (int *) malloc(array_size)));
     memcpy(device_array, host_array, array_size);
-    
-    int  l = log2(length);
-    for (int d=0; d < l; d++) {
-        prefix_upsweep(device_array, length, d);
+     
+    int l = LOG2(length);
+    unsigned stride = 2;
+    for (; stride<=length; stride<<=1) {
+        prefix_upsweep(device_array, length, stride);
     }
-    for (int d=l; d >= 0; d--) {
-        prefix_downsweep(device_array, length, d);
+    device_array[length-1] = 0;
+  //print_array(device_array, length);
+    for (stride>>=1; stride >= 1; stride>>=1) {
+        prefix_downsweep(device_array, length, stride);
     }
-    
+     
     memcpy(host_array, device_array, array_size);
     free(device_array);
 }
@@ -60,17 +65,18 @@ int main(int argc, char **argv){
     }
     int length = 1 << input_n;
     //printf("length: %d\n", length);
-    
+     
     int array_size = length * sizeof(int);
     int *host_array;
     assert(NULL != (host_array = (int*) malloc(array_size)));
-    
+     
     for (int i=0; i<length; ++i) {
         host_array[i] = 1;
     }
-    
+  //print_array(host_array, length);
+     
     prefix(host_array, length);
-    
+     
     bool not_expected = false;
     for (int i=0; i<length; ++i) {
         if (i != host_array[i]) {
@@ -79,12 +85,10 @@ int main(int argc, char **argv){
         }
     }
     if (not_expected) {
-        for (int i=0; i<length; ++i) {
-            printf("%d ", host_array[i]);
-        }
-        printf("failure!\n");
-    } else { printf("success!\n"); }
-    
+        print_array(host_array, length);
+        printf("\nfailure!\n");
+    } else { printf("\nsuccess!\n"); }
+     
     free(host_array);
 }
 
